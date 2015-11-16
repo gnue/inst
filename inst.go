eae1bc9cef2b40ae3764daa746857ce84e54e34c
gnue <gnue@so-kukan.com>
2015-11-16 11:54:22 +0900

    template でファイル生成してインストール

---
+++ inst.go
@@ -0,0 +1,75 @@
+package inst
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+)
+
+type Template interface {
+	Execute(wr io.Writer, data interface{}) error
+}
+
+type Inst struct {
+	Template Template
+	Dirs     []string
+}
+
+func New(t Template, dirs ...string) *Inst {
+	return &Inst{t, dirs}
+}
+
+func (inst *Inst) InstallPath() string {
+	mkd := ""
+
+	for _, dir := range inst.Dirs {
+		d := os.ExpandEnv(dir)
+
+		finfo, err := os.Stat(d)
+		if err == nil && finfo.IsDir() {
+			return d
+		}
+
+		if mkd == "" && os.IsNotExist(err) {
+			mkd = d
+		}
+	}
+
+	if mkd != "" {
+		err := os.MkdirAll(mkd, 0755)
+		if err == nil {
+			return mkd
+		}
+	}
+
+	return ""
+}
+
+func (inst *Inst) Install(name string, mode os.FileMode, data interface{}) (fname string, err error) {
+	d := inst.InstallPath()
+	if d == "" {
+		err = fmt.Errorf("inst: no install path")
+		return
+	}
+
+	fname = filepath.Join(d, name)
+	f, err := os.Create(fname)
+	if err != nil {
+		return
+	}
+	defer f.Close()
+
+	if data == nil {
+		data = name
+	}
+
+	err = inst.Template.Execute(f, data)
+	if err != nil {
+		return
+	}
+
+	err = os.Chmod(fname, mode)
+
+	return
+}
