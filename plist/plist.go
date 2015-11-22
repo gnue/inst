f46a2a9b71b71d0914d0937828e8771d71fe5451
gnue <gnue@so-kukan.com>
2015-11-22 15:17:39 +0900

    plist の self-close 問題を修正

--- plist/plist.go
+++ plist/plist.go
@@ -2,6 +2,7 @@ package plist
 
 import (
 	"io"
+	"regexp"
 
 	"github.com/DHowett/go-plist"
 )
@@ -15,7 +16,24 @@ func (t *Template) Execute(w io.Writer, data interface{}) (err error) {
 		return
 	}
 
+	b = fixBoolTag(b)
 	_, err = w.Write(b)
 
 	return
+}
+
+var (
+	reBoolTag = regexp.MustCompile("<true></true>|<false></false>")
+	trueTag   = []byte("<true/>")
+	falseTag  = []byte("<false/>")
+)
+
+func fixBoolTag(xml []byte) []byte {
+	return reBoolTag.ReplaceAllFunc(xml, func(tag []byte) []byte {
+		if tag[1] == 't' {
+			return trueTag
+		}
+
+		return falseTag
+	})
 }
